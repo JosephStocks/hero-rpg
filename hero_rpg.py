299608f5ce7067eebd84c6f934278f5c374bd312
Joseph Stocks <jpstocks63@gmail.com>
2020-11-19 14:56:13 -0600

    working exactly like the instructions

--- hero_rpg.py
+++ hero_rpg.py
@@ -5,59 +5,60 @@
 # 1. fight goblin
 # 2. do nothing - in which case the goblin will attack him anyway
 # 3. flee
-
-class Hero: # Idea: could create a person class that hero and goblin inherit from OR
-            # OR I could create a Hero class then a villain class that the Goblin inherits from
-    def __init__(self, health, power, name):
+class Character:
+    def __init__(self, isYou, health, power):
+        self.isYou = isYou
         self.health = health
         self.power = power
         self.name = name
-    
-    def attack(self, enemy):
-        enemy.health -= self.power
-        print(f"You do {self.power} damage to the {enemy.__class__.__name__.lower()}.")
-        if enemy.health <= 0:
-            print(f"The {enemy.__class__.name} is dead.")
 
     def alive(self):
         return self.health > 0
 
-    def print_status(self):
-        print(f"You have {self.health} health and {self.power} power.")
-
-class Goblin:
-    def __init__(self, health, power):
-        self.health = health
-        self.power = power
-
     def attack(self, enemy):
         enemy.health -= self.power
-        print(f"The goblin does {self.power} damage to you.")
-        if enemy.health <= 0:
-            print(f"You are dead.")
+        if self.isYou:
+            enemy_name = enemy.__class__.__name__.lower()
+            print(f"You do {self.power} damage to the {enemy_name}.")
+            if enemy.health <= 0:
+                print(f"The {enemy_name} is dead.")
+        else:
+            character_name = self.__class__.__name__.lower()
+            print(f"The {character_name} does {self.power} damage to you.")
+            if enemy.health <= 0:
+                print(f"You are dead.")
 
-    def alive(self):
-        return self.health > 0
-    
     def print_status(self):
-        print(f"The {self.__class__.__name__.lower()} has {self.health} health and {self.power} power.")
+        if self.isYou:
+            print(f"You have {self.health} health and {self.power} power.")
+        else:
+            character_name = self.__class__.__name__.lower()
+            print(f"The {character_name} has {self.health} health and {self.power} power.")
+
+
+class Hero(Character): 
+    pass
+
+
+class Goblin(Character):
+    pass
+
 
 def main():
-    hero = Hero(health=10, power=5, name='you')
-    goblin = Goblin(health=6, power=2)
+    hero = Hero(isYou=True, health=10, power=5)
+    goblin = Goblin(isYou=False, health=6, power=2)
 
     while goblin.alive() and hero.alive():
         hero.print_status()
         goblin.print_status()
-        print()
-        print("What do you want to do?")
+
+        print("\nWhat do you want to do?")
         print("1. fight goblin")
         print("2. do nothing")
         print("3. flee")
         print("> ", end=' ')
         raw_input = input()
         if raw_input == "1":
-            # Hero attacks goblin
             hero.attack(goblin)
         elif raw_input == "2":
             pass
@@ -68,7 +69,6 @@ def main():
             print(f"Invalid input {raw_input}")
 
         if goblin.health > 0:
-            # Goblin attacks hero
             goblin.attack(hero)
 
 main()
